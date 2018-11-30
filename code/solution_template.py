2fafb6d40169d3fdcc4a73c0e9f63da80f28204e
Zach Bernstein <zebernst@gmail.com>
2018-11-29 23:24:44 -0500

    add sample solution.py content

---
+++ code/solution_template.py
@@ -0,0 +1,17 @@
+from code import aoc
+from code.utils import *
+
+
+def part1(data: str):
+    pass
+
+
+def part2(data: str):
+    pass
+
+
+if __name__ == '__main__':
+    data = aoc.data(day=None)
+
+    print(part1(data))
+    print(part2(data))
