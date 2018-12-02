45df83a49822e0e573305d1b958376e41e619acc
Zach Bernstein <zebernst@gmail.com>
2018-12-02 00:50:29 -0500

    add more error handling to aoc.data function

--- code/aoc.py
+++ code/aoc.py
@@ -1,12 +1,17 @@
 from pathlib import Path
 
 
-def data(*, day):
+def data(*, day: int):
     aoc_dir = Path(__file__).resolve().parent.parent
+
+    if not isinstance(day, int):
+        print(f"{day} is not a number!")
+        exit(1)
 
     try:
         with open(aoc_dir / "input" / f"{day:02d}.txt") as f:
             return f.read()
     except Exception as e:
+        print(f"Couldn't load data for day {day}.")
         print(e)
         exit(1)
