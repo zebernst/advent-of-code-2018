2eeec6d7b18b47abfcd6435263be7fcd2fbff423
Zach Bernstein <zebernst@gmail.com>
2018-12-02 00:45:57 -0500

    optimized day 2

--- code/02/solution.py
+++ code/02/solution.py
@@ -1,7 +1,6 @@
 from code import aoc
 from code.utils import *
 from itertools import groupby
-from difflib import SequenceMatcher
 
 
 def part1(data: str):
@@ -12,19 +11,20 @@ def part1(data: str):
             twos += 1
         if [e for e in groups if len(e) == 3]:
             threes += 1
+
     return twos * threes
 
 
 def part2(data: str):
-    ids = [e for e in data.strip().split()]
-    ratios = []
+    ids = data.strip().split()
+    common = []
     for i1, id1 in enumerate(ids):
         for i2, id2 in enumerate(ids):
             if i1 == i2:
                 continue
-            ratios.append((i1, i2, SequenceMatcher(None, id1, id2).ratio()))
-    match = max(ratios, key=lambda t: t[2])
-    return cat(ch for i, ch in enumerate(ids[match[0]]) if ids[match[1]][i] == ch)
+            common.append(cat(ch for i, ch in enumerate(id1) if id2[i] == ch))
+
+    return max(common, key=len)
 
 
 if __name__ == '__main__':
